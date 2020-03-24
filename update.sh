ae66d53c9ceda9b10f7292bd0ae6c293c446ff87
Frank Eggen <eggen@emden.net>
2020-03-24 11:56:47 +0100

    update: fix firefox crash

--- update.sh
+++ update.sh
@@ -6,8 +6,17 @@
 # Version 1.0 21.03.2020
 
 cd ~/newsroom/
+
+# Kill Browser
+killall -w firefox-esr
+sleep 2
+
 git --no-optional-locks reset -q --hard
 git checkout covid19
 git pull
+
+nohup firefox 'index01.html' 2> /dev/null &
+sleep 5
+xdotool search --sync --onlyvisible --class "Firefox" windowactivate key F11
 
 exit 0
